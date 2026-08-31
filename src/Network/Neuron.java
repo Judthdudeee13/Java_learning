ba45930594cda7cf92361c288110df10b1617734
Judthdudeee13 <judthdudeee13@gmail.com>
2026-08-30 21:55:51 -0400

    save

--- src/Network/Neuron.java
+++ src/Network/Neuron.java
@@ -154,8 +154,16 @@ class Network{
         return getOutput();
     }
 
-    public double[][] getWeightsAndBiases(){
-        double[][] returns = new double[2][];
+    public double[][][][] getWeightsAndBiases(){
+        double[][][][] returns = new double[2][][][];
+        double[][][] biases = new double[this.neurons.length-1][][];
+        for (int layer = 1; layer <= this.neurons.length-1; layer++){
+            biases[layer] = new double[this.neurons[layer].length][];
+            for (int neuron = 0; neuron < this.neurons[layer].length-1; neuron++){
+                biases[layer][neuron] = this.neurons[layer][neuron].bias;
+            }
+        }
+        returns[0] = this.weights;
         return returns;
     }
 }
